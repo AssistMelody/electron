c36cbf85bcc443ed8d29aa87d442e7f297977b74
AssistMelody <s41018219961010@gmail.com>
2019-08-13 23:49:35 +0800

    测试

--- src/renderer/class/BaseClass.js
+++ src/renderer/class/BaseClass.js
@@ -3,8 +3,8 @@ export class BaseAttrClass {
     /* 
     *基础属性
     */
-    power   = 0; // 力
-    body    = 0; // 体
+    Power   = 0; // 力
+    Body    = 0; // 体
     Agile   = 0; // 敏
     Skill   = 0; // 术
     Mana    = 0; // 念
